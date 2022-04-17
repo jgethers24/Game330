a0a7bb6c78e23106c3912e6aece4f84da36138d2
jgethers24 <98632512+jgethers24@users.noreply.github.com>
2022-04-17 14:09:11 -0400

    Camera Control script
    This is a script I used to change the cameras angle in the pickmeup lab. it could be adapted for our purposes.

---
+++ Item_Hammer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Hammer : ItemBase
+{
+   
+    protected override void OnItemUsed(PlayerActionScript playerActionScript)
+    {
+
+        
+
+        if(Camera.main.transform.rotation== Quaternion.Euler(90.0f, 0.0f, 0.0f))
+        {
+            Camera.main.transform.rotation = Quaternion.Euler(36.864f, 0.0f, 0.0f);
+            Camera.main.transform.position = new Vector3(0, 9.09f, -12.52f);
+        }
+        else if (Camera.main.transform.rotation == Quaternion.Euler(36.864f, 0.0f, 0.0f))
+        {
+            Camera.main.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+            Camera.main.transform.position = new Vector3(0, 17.5f, 0);
+        }
+
+    }
+ 
+
+}
+
